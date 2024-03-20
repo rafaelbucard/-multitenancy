e4f4f1bb6b23a462838f189433e1b8cf2126ce56
Rafael Buçard <rafaelbucard@gmail.com>
2024-03-20 12:00:09 -0300

    feat:update routes

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,13 @@ Route::get('/', function () {
 Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::resource('/clients', ClientController::class);
     Route::get('/sales', [SaleController::class, 'index']);
+
 });
 
 require __DIR__.'/auth.php';
